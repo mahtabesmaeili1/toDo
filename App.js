54ff14844f5a19f8fe36f35365700dcd0362bb6e
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-01-31 18:52:10 +0100

    added the task components and the title in the app js

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import Task from "./components/Task";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}> Today's Tasks</Text>
+      </View>
+      <View style={styles.items}>
+        {/* this is where the tasks will go */}
+        <Task />
+      </View>
     </View>
   );
 }
@@ -13,8 +18,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "pink",
   },
+  tasksWrapper: {
+    paddingTop: 88,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {},
 });
