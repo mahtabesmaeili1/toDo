bf2274c4e313cad355ac891bb3ab67b4a2e2b1ce
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-01-31 18:31:48 +0100

    wannna do it again

--- App.js
+++ App.js
@@ -1,29 +1,20 @@
-import React, { useState } from "react";
-import TaskForm from "./components/TaskForm";
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [tasks, setTasks] = useState([]);
-
-  function addTask(title) {
-    setTasks([...tasks, { title, id: Math.random().toString() }]);
-  }
-
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <TaskForm onAddTask={addTask} />
-      {tasks.map((task) => (
-        <View key={task.id}>
-          <Text>{task.title}</Text>
-        </View>
-      ))}
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
